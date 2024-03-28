294efbe6f2493ffdb05697f0083bd46b3c669def
dioo1461 <dioo1461@naver.com>
2024-03-28 12:28:41 +0900

    Chore: Navigation 초기설정

--- one_bucket/index.js
+++ one_bucket/index.js
@@ -6,4 +6,18 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import {ThemeProvider} from 'styled-components';
+import theme from 'styles/theme';
+import Navigator from './Navigator';
+
+const ProvidedNavigator = () => {
+  return (
+    <ThemeProvider theme={{...theme}}>
+      <Navigator />
+    </ThemeProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => ProvidedNavigator);
